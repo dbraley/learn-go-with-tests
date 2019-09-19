b4b6f2ff02ffcb32a4d372b013a883ebbb8d8b55
David Braley <davidbraley@khanacademy.org>
2019-09-19 16:47:03 -0400

    Add Triangle, refactor tests for clarity

--- smi/geometry.go
+++ smi/geometry.go
@@ -18,6 +18,12 @@ type Circle struct {
 	Radius float64
 }
 
+// Triangle has a base and height
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 // Perimeter of a rectangle
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
@@ -31,4 +37,9 @@ func (r Rectangle) Area() float64 {
 // Area of a Circle
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+// Area of a Triangle
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
 }

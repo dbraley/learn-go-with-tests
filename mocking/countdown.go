1ad458e803ceae922a5c310bba451800ec869ad4
David Braley <davidbraley@khanacademy.org>
2019-09-24 17:01:32 -0400

    Use Sleeper interface to mock timer

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,16 +4,35 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
+// Sleeper allows for a method to sleep for a set amount of time
+type Sleeper interface {
+	Sleep()
+}
+
+// DefaultSleeper which defaults to sleeping 1 second
+type DefaultSleeper struct{}
+
+// Sleep 1 second
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 // Countdown counts down from 3
-func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }

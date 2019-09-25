4032437dc701d03e243a7f3e2ad983059e7c9d91
David Braley <davidbraley@khanacademy.org>
2019-09-25 14:44:48 -0400

    Make timeout Configurable

--- select/racer.go
+++ select/racer.go
@@ -6,14 +6,19 @@ import (
 	"time"
 )
 
-// Racer races the ping of two web sites
+// Racer races the ping of two web sites with a default timeout of 10 seconds
 func Racer(a, b string) (string, error) {
+	return ConfigurableRacer(a, b, 10*time.Second)
+}
+
+// ConfigurableRacer allows setting of a timeout
+func ConfigurableRacer(a, b string, timeout time.Duration) (string, error) {
 	select {
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeout):
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
